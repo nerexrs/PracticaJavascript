9a1b5e0292090dbec2da299821fd7c60a2a069eb
nerex_rs <ilive4cartoons@gmail.com>
2022-09-22 23:12:07 -0500

    dos numeros mayor, menor o igual

---
+++ MayorMenor.js
@@ -0,0 +1,22 @@
+function mayorMenor() {
+    let num1 = document.getElementById("num1");
+    let valueNum1 = num1.value
+    valueNum1 = Number(valueNum1);
+    let num2 = document.getElementById("num2");
+    let valueNum2 = num2.value;
+    valueNum2 = Number(valueNum2);
+    let string = document.getElementById("string");
+
+    if (valueNum1 > valueNum2) {
+        string.innerText = `${valueNum1} es mayor que ${valueNum2}`;
+    } else if (valueNum1 < valueNum2) {
+        string.innerText = `${valueNum1} es menor que ${valueNum2}`;
+    } else if (valueNum1 == valueNum2) {
+        string.innerText = `${valueNum1} es menor que ${valueNum2}`;
+    } 
+    else {
+        string.innerText = `ingresa un valor valido`;
+    }
+
+
+}
---
+++ prueba.js
@@ -0,0 +1,3 @@
+var condicion = "7" 
+let condicion2 =  7
+console.log(condicion);
